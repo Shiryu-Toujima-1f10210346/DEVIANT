90e8ed2230a79122edbd40cdfd1d480f6fcf51d3
東島子竜 <s1f102103460@iniad.org>
2022-11-26 02:06:38 +0900

    refine Login and SignUp page
    仮置きだっぴ

--- chat/views.py
+++ chat/views.py
@@ -5,3 +5,5 @@ def index(request):
     return render(request, "chat/index.html")
 def signup(request):
     return render(request, "chat/signup.html")
+def login(request):
+    return render(request, "chat/login.html")
