6786f8d66b998fc5d0629f12afde05a97fd9b646
東島子竜 <s1f102103460@iniad.org>
2022-11-30 21:52:04 +0900

    色々
    サイト仮置き
    ヘッダー設置
    ログインとサインアップページのブラッシュアップ

--- chat/urls.py
+++ chat/urls.py
@@ -5,4 +5,11 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("signup/", views.signup, name="singup"),
     path("login/", views.login, name="login"),
+    path("home/", views.home, name="home"),
+    path("others_prof/", views.others_prof, name="others_prof"),
+    path("my_prof/", views.my_prof, name="my_prof"),
+    path("post/", views.post, name="post"),
+    path("tos/", views.tos, name="tos"),
+    path("forgotpass/", views.forgotpass, name="forgotpass"),
+    path("chat/", views.chat, name="chat"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -7,3 +7,17 @@ def signup(request):
     return render(request, "chat/signup.html")
 def login(request):
     return render(request, "chat/login.html")
+def home(request):
+    return render(request, "chat/home.html")
+def others_prof(request):
+    return render(request, "chat/others_prof.html")
+def my_prof(request):
+    return render(request, "chat/my_prof.html")
+def post(request):
+    return render(request, "chat/post.html")
+def tos(request):
+    return render(request, "chat/tos.html")
+def forgotpass(request):
+    return render(request, "chat/forgotpass.html")
+def chat(request):
+    return render(request, "chat/chat.html")
