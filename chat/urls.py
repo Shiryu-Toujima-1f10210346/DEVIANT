4019597ea34a8a62c69158df78770d49a16820d8
東島子竜 <s1f102103460@iniad.org>
2022-12-08 06:36:17 +0900

    色々　詳細はDescriptonに
    ソート機能の実装
    -投稿の人気順､お気に入り降順､お気に入り昇順
    
    投稿の詳細を見れるように
    
    ヘッダーの調整
    -白から黒に
    
    登録画面の一新
    -Bootstrapとtailwindが競合してたので全部やりなおした

--- chat/urls.py
+++ chat/urls.py
@@ -4,12 +4,14 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("signup/", views.signup, name="singup"),
+    path("signupold/", views.signupold, name="singup"),
     path("login/", views.login, name="login"),
     #path("home/", views.home, name="home"),
-    path("home/", views.PostListView.as_view(), name="home"), #投稿の一覧を表示するビュー
+    path("home/", views.home, name="home"), 
     path("others_prof/", views.others_prof, name="others_prof"),
+    path("home/post/<int:pk>/", views.detail, name="detail"),
     path("my_prof/", views.my_prof, name="my_prof"),
-    path("post/", views.PostCreateView.as_view(), name="post"),
+    path("post/", views.post, name="post"),
     path("tos/", views.tos, name="tos"),
     path("forgotpass/", views.forgotpass, name="forgotpass"),
     path("chat/", views.chat, name="chat"),
--- chat/views.py
+++ chat/views.py
@@ -6,6 +6,17 @@ from django.views.generic import View, TemplateView, ListView,CreateView, Detail
 class PostListView(ListView):
     template_name = 'chat/home.html'
     model = Post
+    def get_queryset(self):
+        if ("order" in self.request.GET):
+            if self.request.GET["order"] == "popular":
+                return Post.objects.order_by("-fav_num")
+            elif self.request.GET["order"] == "new":
+                return Post.objects.order_by("-created_at")
+            elif self.request.GET["order"] == "veiled":
+                return Post.objects.order_by("fav_num")
+            else:
+                return Post.objects.order_by("-created_at")
+        return Post.objects.order_by("-created_at")
 home = PostListView.as_view()
 
 #Postモデルを使って投稿を作成するビュー
@@ -15,6 +26,13 @@ class PostCreateView(CreateView):
     fields = ('author', 'title', 'content')
     success_url = '/home/'
 post = PostCreateView.as_view()
+
+#投稿の詳細を表示するビュー
+class PostDetailView(DetailView):
+    template_name = "chat/detail.html"
+    model = Post
+detail = PostDetailView.as_view()
+
 # simple url define
 def index(request):
     return render(request, "chat/index.html")
@@ -22,8 +40,6 @@ def signup(request):
     return render(request, "chat/signup.html")
 def login(request):
     return render(request, "chat/login.html")
-def home(request):
-    return render(request, "chat/home.html")
 def others_prof(request):
     return render(request, "chat/others_prof.html")
 def my_prof(request):
@@ -38,3 +54,5 @@ def chat(request):
     return render(request, "chat/chat.html")
 def room(request, room_name):
     return render(request, "chat/room.html", {"room_name": room_name})
+def signupold(request):
+    return render(request, "chat/signup-old.html")
