be707e1801ea815fed0cbf5eedaea1a73126355d
東島子竜 <s1f102103460@iniad.org>
2023-01-12 17:09:37 +0900

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 urlpatterns = [
     path("", views.index, name="index"),
     path("signup/", views.AccountRegistration.as_view(), name="signup"),
@@ -31,3 +33,8 @@ urlpatterns = [
     ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
